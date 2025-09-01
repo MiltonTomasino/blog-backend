9772046747d591da4a4b6ac46123b426ff325fc5
MiltonTomasino <jonathantomasino13@gmail.com>
2025-09-01 15:26:49 -0700

    Add Cors For Frontend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const blogRouter = require("./routes/blog");
 const userRouter = require("./routes/user");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/blog", blogRouter);
 app.use("/user", userRouter);
